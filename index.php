7f6e1b6eb4ac2d2fe5c065be83c18bfd08286efd
Marlon Rebouças <36989583+marlonreb@users.noreply.github.com>
2021-03-11 17:30:32 -0300

    ajuste
    ajuste index

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 
-    
-
-
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="assets/css/style.css">
-
 <section class="ativo_popup" id="popup_section">
     <div class="popup">
         <div class="poupu_container">
